a9a3e90e7af3761268cf415b8ab6547ad8e92437
Arsalan Patel <arsalan.patel@ximkart.com>
2025-05-18 22:22:29 +0530

    updated for ssl error

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,10 @@
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 from ..core.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
+client = AsyncIOMotorClient(
+    settings.MONGO_URI,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=5000
+)
 db = client.aryng
